b074a140bb41f9b5ade90a6253bb051050585b1c
Thanajsil Janthachot <thanajsilrock@gmail.com>
2025-11-10 20:54:20 +0700

    Locator Methods with Options in Playwright (has, hasNot, hasText, hasNotText Explained)

--- tests/2.locator.spec.ts
+++ tests/2.locator.spec.ts
@@ -13,3 +13,17 @@ test('Practice of locator method', async ({page}) => {
     //await page.locator("id=add-to-cart-sauce-labs-backpack").click();
     await page.locator("data-test=add-to-cart-sauce-labs-backpack").click();
 });
+
+test('Practice oo locator method with options', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('.form_group', {has: page.locator('input#user-name')}).click();
+    await page.locator('.form_group', {has: page.locator('input#user-name')}).pressSequentially('standard_user');
+
+    await page.locator('.form_group', {hasNot: page.locator('input#user-name')}).click();
+    await page.locator('.form_group', {hasNot: page.locator('input#user-name')}).pressSequentially('ssecret_sauce');
+
+    await page.locator('#login-button').click();
+
+    await page.locator('//a', {hasText: 'Sauce Labs Backpack'}).click();
+    await page.locator('.inventory_item_name', {hasNotText: /Sauce.*/}).click();
+});
